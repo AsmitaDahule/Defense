972bd49268ad5b33aa7cec629fd7c70d39dd5ac8
Asmita Dahule <dahuleasmita@gmail.com>
2025-11-12 10:58:09 +0530

    filter, reduce, map

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,80 @@
+const arr =  [5,1,18 , 1,1];
+
+// function multiply(x) {
+//   return 2 * x;
+// }
+
+// console.log(multiply(arr));
 
 
+// // const ans = arr.filter((x) => {
+// //   return x > 2;
+// // })
 
-const arr =  [2,5,8,7,4];
-
-function multiply(x) {
-  return 2 * x;
-}
-
-console.log(multiply(arr));
+// console.log(arr.filter((x) => {
+//   return x > 2;
+// }));
 
 
-const ans = arr.map((x) => {
-  return 4 * x;
-})
+// // const ans = arr.map(multiply);
 
-console.log(ans);
+// console.log(arr.map(multiply));
 
+
+// console.log(arr.filter((x) => {
+//   return x % 2 === 0;
+// }));
+
+
+// console.log(arr.reduce((acc, curr) => {
+//   return acc + curr;
+// }, 0));
+
+
+// function max(arr) {
+//   let maxval = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > maxval) {
+//       maxval = arr[i];
+//     } 
+// } return maxval;
+
+// }
+
+// console.log(max(arr));
+
+// const output = arr.reduce((maxval, curr) => {
+//   if(curr > maxval) {
+//     maxval = curr;
+//   }
+
+//   return maxval;
+// })
+
+// console.log(output)
+
+const users = [
+  {firstName: "John", lastName: "Doe", age: 25},
+  {firstName: "Jane", lastName: "Smith", age: 30},
+  {firstName: "Emily", lastName: "Johnson", age: 22},
+  {firstName: "Michael", lastName: "Brown", age: 35}
+];
+
+const output = users.map((x) => {
+  return x.firstName + " " + x.lastName;
+});
+
+console.log(output);
+
+
+const ageOutput = users.filter((x) => {
+  return x.age >= 25;
+});
+
+console.log(ageOutput);
+
+const totalAge = users.reduce((acc, curr) => {
+  return acc + curr.age;
+} , 0);
+
+console.log(totalAge);
