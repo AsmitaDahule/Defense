5ff8ddbd92cad2b213800013a503ef514f327987
Asmita Dahule <dahuleasmita@gmail.com>
2025-11-12 09:42:08 +0530

    map

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript";
 
-document.getElementById('demo').innerHTML = 'JavaScript';
+
+
+const arr =  [2,5,8,7,4];
+
+function multiply(x) {
+  return 2 * x;
+}
+
+console.log(multiply(arr));
+
+
+const ans = arr.map((x) => {
+  return 4 * x;
+})
+
+console.log(ans);
+
