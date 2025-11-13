f709f90fcc56cab3c4179648f28da16b0d57afd9
Asmita Dahule <dahuleasmita@gmail.com>
2025-11-13 14:31:52 +0530

    const let var

--- JS/script.js
+++ JS/script.js
@@ -53,28 +53,37 @@ const arr =  [5,1,18 , 1,1];
 
 // console.log(output)
 
-const users = [
-  {firstName: "John", lastName: "Doe", age: 25},
-  {firstName: "Jane", lastName: "Smith", age: 30},
-  {firstName: "Emily", lastName: "Johnson", age: 22},
-  {firstName: "Michael", lastName: "Brown", age: 35}
-];
+// const users = [
+//   {firstName: "John", lastName: "Doe", age: 25},
+//   {firstName: "Jane", lastName: "Smith", age: 30},
+//   {firstName: "Emily", lastName: "Johnson", age: 22},
+//   {firstName: "Michael", lastName: "Brown", age: 35}
+// ];
 
-const output = users.map((x) => {
-  return x.firstName + " " + x.lastName;
-});
+// const output = users.map((x) => {
+//   return x.firstName + " " + x.lastName;
+// });
 
-console.log(output);
+// console.log(output);
 
 
-const ageOutput = users.filter((x) => {
-  return x.age >= 25;
-});
+// const ageOutput = users.filter((x) => {
+//   return x.age >= 25;
+// });
 
-console.log(ageOutput);
+// console.log(ageOutput);
 
-const totalAge = users.reduce((acc, curr) => {
-  return acc + curr.age;
-} , 0);
+// const totalAge = users.reduce((acc, curr) => {
+//   return acc + curr.age;
+// } , 0);
 
-console.log(totalAge);
+// console.log(totalAge);
+
+// console.log(c);
+// console.log(b);
+// console.log(a);
+let a;
+var b = 10;
+const c = 2;
+console.log(a);
+console.log(c);
