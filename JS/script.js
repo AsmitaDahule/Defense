6a7058641e46339912b539a4b64a474b27f2c926
Asmita Dahule <dahuleasmita@gmail.com>
2025-11-13 19:55:10 +0530

    setTimeOut

--- JS/script.js
+++ JS/script.js
@@ -81,8 +81,8 @@ const arr =  [5,1,18 , 1,1];
 
 // console.log(c);
 // console.log(b);
-// console.log(a);
-let a;
+console.log(a);
+let a = 20;
 var b = 10;
 const c = 2;
 console.log(a);
---
+++ JS/setTimeOut/index.js
@@ -0,0 +1,11 @@
+x();
+
+function x() {
+  for(let i = 0; i < 6; i++) {
+    setTimeout(function() {
+  console.log(i);
+}, i * 1000);
+  }
+  var a = "asmiii"
+}
+
