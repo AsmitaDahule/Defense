587e8a0eb36e63023b036ac5efeee50667dc3feb
Asmita Dahule <dahuleasmita@gmail.com>
2025-11-14 22:05:50 +0530

    event listener

--- JS/callback/index.js
+++ JS/callback/index.js
@@ -1,10 +1,19 @@
-function x(y) {
-  console.log("Aa");
-  y();
+// function x(y) {
+//   console.log("Aa");
+//   y();
+// }
+
+
+
+// x(function y() {
+//   console.log("yy called");
+// });
+
+function eventListener(){
+  let count = 0;
+  document.getElementById("btn").addEventListener("click", function(){
+  console.log("hieee", ++count);
+})
 }
 
-
-
-x(function y() {
-  console.log("yy called");
-});
+eventListener();
